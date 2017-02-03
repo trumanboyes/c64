19d47a516b51ed41aaa57fbc7ce22d8f2600f542
Truman Boyes <truman@truman.net>
2017-02-02 21:48:34 -0500

    Adding colors

---
+++ colors.asm
@@ -0,0 +1,12 @@
+; dasm source
+	processor 6502
+	
+	org $0801 ; sys2061
+sysline:
+	.byte $0b,$08,$01,$00,$9e,$32,$30,$36,$31,$00,$00,$00
+
+	org $080d ; sys2061
+
+loop:	inc $ffff
+	inc $d021
+	jmp loop
